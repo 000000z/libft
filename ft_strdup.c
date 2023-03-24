ea043647f0b57cf6f3c3d3cf9dc145bd898b742c
000000z <zinabi@zinabi>
2023-03-24 17:19:46 +0100

    l

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,11 +1,24 @@
+#include <stdlib.h>
 char	*ft_strdup(const char *s)
 {
 	int	i;
+	char	*str;
+	int	len;
 
+	len = 0;
 	i = 0;
-	while(s[i])
+	while(s[len])
 	{
-		s2[i] = s[i];
-		i++;
+		len++;
 	}
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (s[1] != '\0')
+	{
+		while(s[i])
+		{
+			str[i] = s[i];
+			i++;
+		}
+	}
+	return(str);
 }
