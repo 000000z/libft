47ad041632201991c57417cb59497a8f2d3efcfe
000000z <zinabi@zinabi>
2023-03-30 00:53:46 +0200

    bug

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,9 +1,10 @@
-int	ft_strcmp(const char *s1, const char *s2)
+#include <stddef.h>
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (s1[i])
+	while (s1[i] && i < n)
 	{
 		if(s1[i] == s2[i])
 			i++;
@@ -12,14 +13,3 @@ int	ft_strcmp(const char *s1, const char *s2)
 	}
 	return(0);	
 }
-/*
-#include <stdio.h>
-#include <string.h>
-int	main(void)
-{
-	const char s1[] = "ve(";
-	const char s2[] = "ver";
-	printf("%d\n", ft_strcmp(s1, s2));
-	printf("%d\n", strcmp(s1, s2));
-	return(0);
-}*/
